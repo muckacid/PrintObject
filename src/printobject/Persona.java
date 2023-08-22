587b5616f04c6a5b47f9657fe8652e60eb2b2ddb
muckacid <moraga.inf@gmail.com>
2023-08-22 15:23:25 -0400

    comprimi los 2 primeros ejemplos en 1 solo

--- src/printobject/Persona.java
+++ src/printobject/Persona.java
@@ -5,12 +5,14 @@
 package printobject;
 
 import java.time.LocalDate;
+import java.time.Period;
 
 /**
  *
  * @author morag
  */
 public class Persona {
+
     private String nombre;
     private LocalDate nacimiento;
 
@@ -37,4 +39,36 @@ public class Persona {
     public void setNacimiento(LocalDate nacimiento) {
         this.nacimiento = nacimiento;
     }
+
+    public Period getAge() {
+        /* 
+            2023 08 21
+            2023 07 31
+            1994 01 25
+        _______________
+              29  6  6 
+                
+        LocalDate hoy = LocalDate.now();
+        LocalDate nacimiento = this.birthday;
+        Period edad = Period.between(nacimiento, hoy);
+        return edad;
+         */
+
+        return Period.between(this.nacimiento, LocalDate.now());
+    }
+
+    @Override
+    public String toString() {
+        String br = "\n";
+        String line = "+----------------------------------------------";
+        String res = "";
+        res += line + br;
+        res += "| Nombre              :" + nombre + br;
+        res += "| Fecha de nacimiento :" + nacimiento + br;
+        res += "| Edad                :" + getAge().getYears() + " anos" + br;
+        res += line + br;
+
+        return res;
+    }
+
 }
--- src/printobject/PrintObject.java
+++ src/printobject/PrintObject.java
@@ -14,24 +14,60 @@ public class PrintObject {
 
     /**
      * @param args the command line arguments
+     *
+     * Esta aplicacion tiene como ojbetivo
+     * ejemplificar la creacion de una clase
+     * para posteriormente instanciar una clase 
+     * y utilizar los atributos de clase a nuestra conveniencia 
+     * utilizar los accesadores(getters) y mutadores(setters)
+     * imprimiendolo de forma efectiva.
+     *
      */
     public static void main(String[] args) {
-        /* 
-            esta aplicacion tiene como ojbetivo 
-            ejemplificar la creacion de una clase
-            para posteriormente instanciar una clase
-            y ejemplificar como utilizar un ojjeto imprimiendolo
-            de forma efectiva.
-            */
+        
+        /*
+        * Definimos en primer lugar los datos que utilizaremos a continuacion
+        * En este bloque generamos la instancia de clase
+        * En este caso utilizamos el metodo toString para imprimir el objeto
+        * Clase nombreDelObjeto = new Constructor(parametros)
+        */
         String nombre = "Maximiliano";
         LocalDate fechaDeNacimiento = LocalDate.of(1994, 1, 25);
-        // Clase nombreDelObjeto = new Constructor(parametros)
         Persona objeto = new Persona(nombre, fechaDeNacimiento);
-    
-        System.out.println("+----------------------------------------------");
-        System.out.println("| Nombre              :" + objeto.getNombre());
-        System.out.println("| Fecha de nacimiento :" + objeto.getNacimiento());
-        System.out.println("+----------------------------------------------");
+        System.out.println(objeto);
+        
+        
+        /*
+        * En este caso llamamos a los atributos de clase y los concatenamos
+        * para generar la estructura que necesitemos.
+        *
+        */
+        System.out.println("+--------------------------------------------+");
+        System.out.println("| "+ objeto.getNombre());
+        System.out.println("| Tu edad es ");
+        System.out.println("+--------------------------------------------+");
+        System.out.println("| Anos   : " + objeto.getAge().getYears());
+        System.out.println("| Meses  : " + objeto.getAge().getMonths());
+        System.out.println("| Dias   : " + objeto.getAge().getDays());
+        System.out.println("+--------------------------------------------+");
+        
+        /*
+        * Si en algun momento deseamos modificar el valor de un atributo
+        * utilizamos los mutadores(Setters) e imprimimos para mostrar el cambio
+        */
+        objeto.setNombre("Maximiliano Moraga");
+        // Podemos imrpimir directamente el nombre
+        System.out.println("[->] " + objeto.getNombre());
+        
+        // O podriamos imprimir el objeto directamente como lo 
+        // vimos anteriormente
+        
+        System.out.println(objeto);
+        
+        
+        
+        
+        
     }
-    
+
 }
